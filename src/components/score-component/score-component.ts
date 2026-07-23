eb6069e65b987f6b6d720882a89f7fa75864f13f
Leo <leolou.90@gmail.com>
2026-07-22 19:12:04 -0700

    add score component

---
+++ src/components/score-component/score-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-score-component',
+  imports: [],
+  templateUrl: './score-component.html',
+  styleUrl: './score-component.css',
+})
+export class ScoreComponent {
+
+}
