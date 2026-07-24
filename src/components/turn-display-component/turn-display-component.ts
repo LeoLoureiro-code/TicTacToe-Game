40f621de7cd8627cb9a3fe9a9a779a66daba737b
Leo <leolou.90@gmail.com>
2026-07-23 19:03:15 -0700

    add components

---
+++ src/components/turn-display-component/turn-display-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-turn-display-component',
+  imports: [],
+  templateUrl: './turn-display-component.html',
+  styleUrl: './turn-display-component.css',
+})
+export class TurnDisplayComponent {
+
+}
