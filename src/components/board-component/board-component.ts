356716eca8c10a450b5c3f18e01b561a316aef4e
Leo <leolou.90@gmail.com>
2026-07-27 19:08:20 -0700

    add imports to board component

--- src/components/board-component/board-component.ts
+++ src/components/board-component/board-component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { SpaceComponent } from '../space-component/space-component';
 
 @Component({
   selector: 'board-component',
-  imports: [],
+  imports: [SpaceComponent],
   templateUrl: './board-component.html',
   styleUrl: './board-component.css',
 })
 export class BoardComponent {
-
+  readonly spaces = Array.from({ length: 6 });
 }
