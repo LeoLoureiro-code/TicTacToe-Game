523a4ea09369235e70cd3883596ff1988a117ed5
Leo <leolou.90@gmail.com>
2026-07-28 18:23:30 -0700

    add new game menu component

---
+++ src/components/new-game-menu-component/new-game-menu-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-game-menu-component',
+  imports: [],
+  templateUrl: './new-game-menu-component.html',
+  styleUrl: './new-game-menu-component.css',
+})
+export class NewGameMenuComponent {
+
+}
