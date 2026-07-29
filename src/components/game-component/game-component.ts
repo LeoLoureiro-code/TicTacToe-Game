5063ab0db8036bffaf93ab5a00c442ae1dacb0b4
Leo <leolou.90@gmail.com>
2026-07-28 18:57:14 -0700

    add game component

---
+++ src/components/game-component/game-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-component',
+  imports: [],
+  templateUrl: './game-component.html',
+  styleUrl: './game-component.css',
+})
+export class GameComponent {
+
+}
