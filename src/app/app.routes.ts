486874992d09a075d85c4fb3053cbb92589fee09
Leo <leolou.90@gmail.com>
2026-07-31 22:22:56 -0700

    add principal routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
+import { NewGameMenuComponent } from '../components/new-game-menu-component/new-game-menu-component';
+import { GameComponent } from '../components/game-component/game-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "", 
+        component: NewGameMenuComponent 
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    },
+    {
+        path:'game',
+        component: GameComponent
+    },
+];
