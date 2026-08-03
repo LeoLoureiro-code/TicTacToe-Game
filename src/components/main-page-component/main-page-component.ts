b76e61b5ab911f6d8e141ccc4bb177f822f75dbf
Leo <leolou.90@gmail.com>
2026-08-02 18:38:16 -0700

    add score components

--- src/components/main-page-component/main-page-component.ts
+++ src/components/main-page-component/main-page-component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { BoardComponent } from '../board-component/board-component';
+import { ScoreComponent } from '../score-component/score-component';
 
 @Component({
   selector: 'main-page-component',
-  imports: [],
+  imports: [BoardComponent, ScoreComponent],
   templateUrl: './main-page-component.html',
   styleUrl: './main-page-component.css',
 })
 export class MainPageComponent {
-
+  readonly scores = Array.from({ length: 3 });
 }
