4a0aca7791e255f27de2652998f7966456126190
Leo <leolou.90@gmail.com>
2026-08-17 18:53:21 -0700

    add game interface to create a game state

--- src/interfaces/game-interface.ts
+++ src/interfaces/game-interface.ts
@@ -1,2 +1,10 @@
+export type Mark = 'X' | 'O' | '';
+
+export type GameResult = 'X' | 'O' | 'draw' | null;
+
 export interface GameInterface {
+  turn: 'X' | 'O';
+  spaces: Mark[][];
+  isGameOver: boolean;
+  result: GameResult;
 }
