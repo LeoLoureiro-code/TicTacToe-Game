e6b01934027ba11bb5dcdd9bdbbf2910ebfbb950
Leo <leolou.90@gmail.com>
2026-08-18 18:53:59 -0700

    add game state object

--- src/services/game-service/game-service.ts
+++ src/services/game-service/game-service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@angular/core';
+import { GameInterface } from '../../interfaces/game-interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GameService {
   
+  gameState: GameInterface = {
+    turn: 'X',
+    spaces: [
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', '']
+    ],
+    isGameOver: false,
+    result: null
+  };
+
+  
+
 }
