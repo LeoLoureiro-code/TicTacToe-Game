98794b07d7552693fe229b64adafbcf9bf611cd3
Leo <leolou.90@gmail.com>
2026-08-20 19:00:44 -0700

    add game service and add figure method

--- src/components/space-component/space-component.ts
+++ src/components/space-component/space-component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { GameService } from '../../services/game-service/game-service';
 
 @Component({
+  standalone: true,
   selector: 'space-component',
-  imports: [],
   templateUrl: './space-component.html',
   styleUrl: './space-component.css',
 })
 export class SpaceComponent {
 
+  figure:string = "";
+
+  constructor(private gameService:GameService){
+
+  }
+
+  AddFigure(){
+    this.figure = this.gameService.AddFigure();
+  }
 }
