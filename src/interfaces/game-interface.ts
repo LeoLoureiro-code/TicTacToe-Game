c7a1ff5f885d18f0c9adfe32fa50553801302995
Leo <leolou.90@gmail.com>
2026-08-30 19:02:30 -0700

    add turn number to interface

--- src/interfaces/game-interface.ts
+++ src/interfaces/game-interface.ts
@@ -4,6 +4,7 @@ export type GameResult = 'X' | 'O' | 'draw' | null;
 
 export interface GameInterface {
   turn: 'X' | 'O';
+  turnNumber: number;
   spaces: Mark[][];
   isGameOver: boolean;
   result: GameResult;
