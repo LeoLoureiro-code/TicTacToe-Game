a1bde3e52a136e5aa1340c2d6f487fa1543a6bb5
Leo <leolou.90@gmail.com>
2026-08-30 18:51:41 -0700

    add modal with if and gets for wins and if game is over from the service

--- src/components/game-component/game-component.ts
+++ src/components/game-component/game-component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { BoardComponent } from "../board-component/board-component";
+import { TurnDisplayComponent } from "../turn-display-component/turn-display-component";
+import { GameService } from '../../services/game-service/game-service';
+import { WinnerModalComponent } from '../winner-modal-component/winner-modal-component';
 
 @Component({
-  selector: 'app-game-component',
-  imports: [],
+  selector: 'game-component',
+  imports: [BoardComponent, TurnDisplayComponent, WinnerModalComponent],
   templateUrl: './game-component.html',
   styleUrl: './game-component.css',
 })
 export class GameComponent {
+
+
+  constructor(private gameService:GameService){}
+
+  get isGameOver(): boolean{
+    return this.gameService.gameState.isGameOver;
+  }
+
+  get crossWins(): number {
+    return this.gameService.crossWins;
+  }
+
+  get circleWins(): number {
+    return this.gameService.circleWins;
+  }
+
+  get drawGames(): number {
+    return this.gameService.drawGames;
+  }
 
 }
