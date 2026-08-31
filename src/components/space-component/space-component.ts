f7a71443546ea49faa079ad0954e3351decd140c
Leo <leolou.90@gmail.com>
2026-08-30 18:54:24 -0700

    change add figure to stop checking for winner

--- src/components/space-component/space-component.ts
+++ src/components/space-component/space-component.ts
@@ -11,20 +11,16 @@ export class SpaceComponent {
 
   isDisabled = false;
   isGameOver = false;
+
   @Input() row!: number;
   @Input() column!: number;
   @Input() index!: number;
   @Input() figure: string = '';
 
   constructor(public gameService:GameService){
-
   }
 
   AddFigure(){
-     this.gameService.AddFigure(this.row, this.column);
-     this.gameService.CheckWinner();
+      this.gameService.AddFigure(this.row, this.column);
   }
-
-
-
 }
