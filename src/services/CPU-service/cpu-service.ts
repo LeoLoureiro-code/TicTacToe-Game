b3fb85fc7d308c5522f72a37c49901d4ea2112b0
Leo <leolou.90@gmail.com>
2026-09-01 18:58:34 -0700

    add get available spaces function

--- src/services/CPU-service/cpu-service.ts
+++ src/services/CPU-service/cpu-service.ts
@@ -1,8 +1,46 @@
 import { Injectable } from '@angular/core';
+import { GameService } from '../game-service/game-service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CPUService {
+
   
+
+constructor(private gameService: GameService){}
+  
+  
+GetAvailableSpaces(){
+
+  const availableSpaces = [];
+
+  for (let row = 0; row < 3; row++) {
+  for (let column = 0; column < 3; column++) {
+
+    if (this.gameService.gameState.spaces[row][column] === '') {
+      availableSpaces.push([row, column]);
+    }
+    
+    const randomSpace =
+    availableSpaces[Math.floor(Math.random() * availableSpaces.length)];
+
+    this.gameService.AddFigure(randomSpace[0], randomSpace[1]);
+  }
+}
+
+}
+
+// GetRandomMove(){
+
+// }
+
+// GetBestMove(){
+
+// }
+
+// Minimax(){
+
+// }
+
 }
